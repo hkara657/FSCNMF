5130ab6ad55eb7077dd7880e37ac95637e3551d9
Harsh Kara <harsh.kara@csa.iisc.ernet.in>
2018-03-05 15:16:27 +0530

    Delete abc.py

--- abc.py
+++
@@ -1,6 +0,0 @@
-import sys
-print "This is the name of the script: ", sys.argv[0]
-print "Number of arguments: ", len(sys.argv)
-l = len(sys.argv)
-for i in range(1,l):
-	print '###',int(sys.argv[i])+100
